3d395eba578e0da3a0c0ffd4392a3d9df77bf628
Usama Jamil <usamajamil77@gmail.com>
2021-04-14 20:40:44 +0000

    add unit test

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# import numpy
+
 print("numpy")
 # test_capitalize.py
 
 def capital_case(x):
     return x.capitalize()
 
-def test_capital_case():
-    assert capital_case('semaphore') == 'Semaphore'
+def 
+
--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
-
-def capital_case(x):
-    return x.capitalize()
+import main as m
+# def capital_case(x):
+#     return x.capitalize()
 
 def test_capital_case():
-    assert capital_case('semaphore') == 'Semaphore'
+    assert m.capital_case('semaphore') == 'Semaphore'
